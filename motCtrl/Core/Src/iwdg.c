e6c5d66b8a86cff2f64a6995f5e28d92b7296b3a
DESKTOP-ROOGGBN\Smersh <dbtjdtn325@gmail.com>
2026-03-14 14:17:29 +0900

    6.2ms 마다 리셋되는 워치독 완성

--- motCtrl/Core/Src/iwdg.c
+++ motCtrl/Core/Src/iwdg.c
@@ -13,7 +13,8 @@ bool iwdg_init(typIwdg_Prescaler prescaler, uint16_t reload_val)
 {
     vIwdg_handler.inst = IWDG;
 
-    // 1. CMSIS 레지스터 제어 시작
+    // CMSIS 레지스터 제어 시작
+    vIwdg_handler.inst->KR = IWDG_KEY_START;
     vIwdg_handler.inst->KR = IWDG_KEY_ENABLE_ACCESS; // 쓰기 보호 해제
 
     if(prescaler > IWDG_DIV_256)
@@ -28,13 +29,15 @@ bool iwdg_init(typIwdg_Prescaler prescaler, uint16_t reload_val)
     // 레지스터 업데이트 대기 (상태 레지스터 SR의 PVU, RVU 비트가 0이 될 때까지)
     while((vIwdg_handler.inst->SR & (IWDG_SR_PVU | IWDG_SR_RVU)) != 0); // 윈도우비트는 윈도우 자체를 안쓰니까 업데이트 X
 
+    vIwdg_handler.inst->KR = IWDG_KEY_RELOAD; // 세팅한 782 값을 실제 카운터에 밀어 넣기
+
     vIwdg_handler.isInit = true;
     return true;
 }
 
 bool iwdg_start(void)
 {
-    if(vIwdg_handler.inst == NULL || vIwdg_handler.isInit == false) return false;
+    if(vIwdg_handler.isInit == false) return false;
     
     // 워치독 타이머 가동 -> 이후로는 못 멈춤
     vIwdg_handler.inst->KR = IWDG_KEY_START; 
